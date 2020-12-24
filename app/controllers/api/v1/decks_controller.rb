18cfe0d4b4af18705c610b5cf85067ee526d0b48
rtetelbaum <rtetelbaum@mailbox.org>
2020-12-23 21:59:52 -0500

    fixed params for decks

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -30,7 +30,7 @@ class Api::V1::DecksController < ApplicationController
 	private
     
 	def deck_params 
-		params.permit(:title, :subject, :complete, :count)
+		params.permit(:title, :subject, :completed, :count, :user_id)
 	end
 
 end
